a4e7583248342699061ac0c2d10885677fd0525d
cazicss <31048700+cazicss@users.noreply.github.com>
2018-12-14 20:06:08 -0600

    override getlettergrade(double)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +11,31 @@ namespace GradeBook.GradeBooks
         // constructor
         public RankedGradeBook(string name) : base(name){
             Type = GradeBookType.Ranked;
+        }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            // not enough students to generate a proper curve
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            // how many students in each of the 5 brackets
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+            // create descending ordered list of student grades
+            var grades =
+                Students
+                .OrderByDescending(student => student.AverageGrade)
+                .Select(student => student.AverageGrade)
+                .ToList();
+
+            // calculate grade from ranked brackets
+            if (averageGrade >= grades[threshold - 1]) return 'A';
+            else if (averageGrade >= grades[threshold * 2 - 1]) return 'B';
+            else if (averageGrade >= grades[threshold * 3 - 1]) return 'C';
+            else if (averageGrade >= grades[threshold * 4 - 1]) return 'D';
+            else return 'F';
         }
 
     }
